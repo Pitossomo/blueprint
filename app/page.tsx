d80f416e2ded1246b9faaa01e642f496c3da2606
Pitossomo <pedro.h.teles@hotmail.com>
2024-12-12 16:52:21 -0300

    feat(page.tsx): Change shapes to rectangle representing the slab

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { useState, useRef, useEffect } from "react";
+import { Slab } from "@/classes/slab";
+import { useState, useRef, useEffect, ChangeEvent } from "react";
 
 export default function Home() {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState<string>('');
+  const [slabs, setSlabs] = useState<Slab[]>([]);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
@@ -13,36 +15,28 @@ export default function Home() {
     if (canvas && ctx) {
       // Clean the canvas
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      
-      // Parse the input string and draw shapes
-      const shapes = inputValue.split(',').map(shape => shape.trim());
-      console.log(shapes)
-      
-      shapes.forEach((shape, index) => {
-        const [type, x, y, size] = shape.split(' ');
-        switch (type) {
-          case 'circle':
-            ctx.beginPath();
-            ctx.arc(parseFloat(x),parseFloat(y), parseFloat(size), 0, Math.PI * 2);
-            ctx.fill();
-            break;
-          case 'square':
-            ctx.fillRect(parseFloat(x), parseFloat(y), parseFloat(size), parseFloat(size));
-            break;
-          case 'triangle':
-            ctx.beginPath();
-            ctx.moveTo(parseFloat(x), parseFloat(y));
-            ctx.lineTo(parseFloat(x) + parseFloat(size), parseFloat(y));
-            ctx.lineTo(parseFloat(x) + (parseFloat(size) / 2), parseFloat(y) - parseFloat(size));
-            ctx.closePath();
-            ctx.fill();
-          break;
-          default:
-            break;
-        }
-      });
+
+      slabs.forEach((slab) => { slab.draw(ctx) })
+
     }
   }, [inputValue]);
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>): void => {
+    const input = e.target.value;
+    setInputValue(input);
+    
+    // Parse input and update customSlabs
+    const newSlabs:Slab[] = []
+    input.split(',').forEach((slab: string) => {
+      try {
+        const [x, y, dx, dy] = slab.trim().split(' ').map(Number);
+        newSlabs.push(new Slab(x, y, dx, dy));  
+      } catch (e) {}
+    });
+    console.log(newSlabs)
+    
+    setSlabs(newSlabs);
+  };
 
   return (
     <div className="p-4">
@@ -51,7 +45,7 @@ export default function Home() {
         className="w-full h-24 p-2 mb-4 border rounded"
         placeholder="Digite as instruções aqui..."
         value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
+        onChange={handleChange}
       />
       <canvas
         ref={canvasRef}
