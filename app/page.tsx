376917cc1b0536a683740951b8767d2fadfcaecb
Pitossomo <pedro.h.teles@hotmail.com>
2024-12-18 17:38:21 -0300

    feat(page.tsx): Add an activeLayer state and change the select input to be state controlled

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { useState, useRef, useEffect, ChangeEvent } from "react";
 export default function Home() {
   const [elementsManager, setElementsManager] = useState<ElementsManager>(new ElementsManager());
   const [inputValue, setInputValue] = useState<string>('');
+  const [activeLayer, setActiveLayer] = useState<LayerType>(elementsManager.activeLayer);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   
   useEffect(() => {
@@ -13,17 +14,25 @@ export default function Home() {
     const ctx = canvas?.getContext('2d');
 
     if (canvas && ctx) {
+      // Set active layer
+      elementsManager.setActiveLayer(activeLayer);
       // Clean the canvas
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       // Call the elements manager to draw on the canvas
       elementsManager.draw(ctx)
     }
-  }, [inputValue]);
+  }, [inputValue, activeLayer]);
 
-  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>): void => {
+  const handleTextChange = (e: ChangeEvent<HTMLTextAreaElement>): void => {
     const input = e.target.value;
     setInputValue(input);
-    elementsManager.parseInput(input)    
+  };
+
+  const handleLayerChange = (e: ChangeEvent<HTMLSelectElement>): void => {
+    const selectedLayer = e.target.value as LayerType;
+    console.log('layer changed')
+    setActiveLayer(selectedLayer)
+    elementsManager.setActiveLayer(selectedLayer);
   };
 
   const layerTypes = Object.values(LayerType);
@@ -38,16 +47,17 @@ export default function Home() {
         className="border border-gray-300 rounded"
       />
 
-      <select className="p-4">
+      <select className="p-4" onChange={handleLayerChange}>
         { layerTypes.map(layer => (
-          <option key={layer}>{layer}</option>
+          <option key={layer} value={layer}>{layer}</option>
         ))}
       </select>
+
       <textarea
         className="w-full h-24 p-2 mb-4 border rounded"
         placeholder="Digite as instruções aqui..."
         value={inputValue}
-        onChange={handleChange}
+        onChange={handleTextChange}
       />
     </div>
   );
