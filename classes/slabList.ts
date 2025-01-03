5bb8b3a93ad18b1a5a7ccf4ffe89023721f27a40
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-03 07:12:22 -0300

    feat(slab.ts): add floor reference on the slab object

--- classes/slabList.ts
+++ classes/slabList.ts
@@ -6,4 +6,9 @@ export class SlabList {
     draw = (ctx: CanvasRenderingContext2D) => {
         this.elements.forEach(el => {el.draw(ctx)})
     }
+
+    add(newSlab: Slab) {
+        this.elements.push(newSlab);
+        this.elements.sort((a, b) => a.x - b.x || a.y - a.y)
+    }
 }
