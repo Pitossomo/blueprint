e3256eda2e6eea6452a8f6b6e0989c06c85e9e23
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-03 07:11:20 -0300

    test(elementsManager): test for layerChange

--- classes/elementsManager.ts
+++ classes/elementsManager.ts
@@ -15,6 +15,15 @@ export class ElementsManager {
         this.activeLayer = LayerType.FLOORS;
         this.floorList = new FloorList();
         this.slabList = new SlabList();
+        
+        // For testing purposes
+        /*
+        let floor0 = new Floor([],0)
+        this.floorList.add(floor0)
+        this.floorList.add(new Floor([],10))
+        this.floorList.add(new Floor([],20))
+        this.slabList.add(new Slab(50,50,300,300,floor0))
+        */
     }
 
     setActiveLayer(layer: LayerType) {
