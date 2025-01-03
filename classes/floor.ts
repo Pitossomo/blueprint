eeb054440f1a4f8754e9cee64c6130291ffc2a4a
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-03 07:11:30 -0300

    test(floor.ts): test for layerChange

--- classes/floor.ts
+++ classes/floor.ts
@@ -3,9 +3,22 @@ import { Point2D } from "./point2D";
 
 export class Floor implements IElement{
     path: Point2D[];
+    height: number;
     
-    constructor(path: Point2D[]) {
+    constructor(path: Point2D[], height: number) {
+        // For testing purposes
+        /*
+        let startPoint = new Point2D(100 + height, 100 + height);
+        this.path = [
+            startPoint,
+            new Point2D(100 + height, 200 + height),
+            new Point2D(200 + height, 200 + height),
+            new Point2D(200 + height, 100 + height),
+            startPoint
+        ];
+        */
         this.path = path;
+        this.height = height;
     }
     
     draw(ctx: CanvasRenderingContext2D) {
