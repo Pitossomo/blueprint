5a032a1730454e11ac1f052295408bfed429b205
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-03 07:11:49 -0300

    feat(floorList.ts): Add 'add' method

--- classes/floorList.ts
+++ classes/floorList.ts
@@ -3,7 +3,12 @@ import { Floor } from "./floor";
 export class FloorList {
     elements: Floor[] = [];
     
-    draw = (ctx: CanvasRenderingContext2D) => {
+    draw(ctx: CanvasRenderingContext2D) {
         this.elements.forEach(el => {el.draw(ctx)})
+    }
+
+    add(newFloor: Floor) {
+        this.elements.push(newFloor);
+        this.elements.sort((a,b) => a.height - b.height)
     }
 }
