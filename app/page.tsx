dea1b52121fc76213f16eadee65f16f7905ccafc
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-04 19:25:34 -0300

    feat(page.tsx): Add Toolbar component to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { ElementsManager, LayerType } from "@/classes/elementsManager";
 import { useState, useRef, useEffect, ChangeEvent } from "react";
+import Toolbar from "./components/toolbar";
 
 export default function Home() {
   const [elementsManager, setElementsManager] = useState<ElementsManager>(new ElementsManager());
@@ -14,12 +15,9 @@ export default function Home() {
     const ctx = canvas?.getContext('2d');
 
     if (canvas && ctx) {
-      // Set active layer
-      elementsManager.setActiveLayer(activeLayer);
-      // Clean the canvas
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      // Call the elements manager to draw on the canvas
-      elementsManager.draw(ctx)
+      elementsManager.setActiveLayer(activeLayer);        // Set active layer
+      ctx.clearRect(0, 0, canvas.width, canvas.height);   // Clean the canvas
+      elementsManager.draw(ctx);                          // Call the elements manager to draw on the canvas
     }
   }, [inputValue, activeLayer]);
 
@@ -38,23 +36,25 @@ export default function Home() {
   const layerTypes = Object.values(LayerType);
 
   return (
-    <div className="p-4">
+    <div className="w-full h-screen p-4">
       <h1 className="text-2xl font-bold mb-4">Desenhe Formas Geométricas</h1>
 
       <canvas
+        className="border border-gray-300 rounded w-full"
+        width="1000" height="300"
         ref={canvasRef}
-        width={800} height={600}
-        className="border border-gray-300 rounded"
       />
 
-      <select className="p-4" onChange={handleLayerChange}>
+      <select className="p-4 border border-gray my-2" onChange={handleLayerChange}>
         { layerTypes.map(layer => (
           <option key={layer} value={layer}>{layer}</option>
         ))}
       </select>
 
+      <Toolbar elementsManager={elementsManager} setElementsManager={setElementsManager} />
+
       <textarea
-        className="w-full h-24 p-2 mb-4 border rounded"
+        className="w-full h-32 p-2 mb-4 border rounded"
         placeholder="Digite as instruções aqui..."
         value={inputValue}
         onChange={handleTextChange}
