1a7cb61d355be542646270a9b007950fb5b36a9c
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-05 14:05:44 -0300

    feat(toolbar.tsx): Add mock function to "Add floor" button

--- app/components/toolbar.tsx
+++ app/components/toolbar.tsx
@@ -1,16 +1,30 @@
-import { ElementsManager } from "@/classes/elementsManager";
-import styles from './Toolbar.module.css';
+import { ElementsManager, LayerType } from "@/classes/elementsManager";
+import { Floor } from "@/classes/floor";
+import { Point2D } from "@/classes/point2D";
+import { IElement } from "../interfaces/iElement";
+import { IElementList } from "../interfaces/iElementList";
 
 type ToolbarProps = {
     elementsManager: ElementsManager;
-    setElementsManager: (em:ElementsManager) => void;
+    activeLayer: LayerType;
 }
 
-export default function Toolbar({elementsManager, setElementsManager}: ToolbarProps) {    
+const newPath = [
+    new Point2D(100,100),
+    new Point2D(500,100),
+    new Point2D(500,300),
+    new Point2D(100,300),
+    new Point2D(100,100),
+]
+
+export default function Toolbar({elementsManager, activeLayer}: ToolbarProps) {
     return (
         <div className="flex justify-left items-center p-2 bg-gray-200">
             <button className="px-4 py-2 text-lg cursor-pointer bg-blue-600 text-white border border-gray-300 rounded transition duration-300 hover:bg-blue-800"
-                onClick={() => {alert("Add new floor")}}
+                onClick={() => { 
+                    elementsManager.addFloor(new Floor(newPath,0));
+                    elementsManager.draw(activeLayer);
+                } }
             >
                 Add Floor
             </button>
