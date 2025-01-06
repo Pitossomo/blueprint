ffa4ccfc134ecf8f10b4e37d69c31e0460c5b5d6
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-06 17:19:02 -0300

    feat(toolbar): Get Layer options from LAYERMAP

--- app/components/toolbar.tsx
+++ app/components/toolbar.tsx
@@ -1,16 +1,16 @@
-import { ElementsManager, LayerType } from "@/classes/elementsManager";
+import { ElementsManager } from "@/classes/elementsManager";
 import { Floor } from "@/classes/floor";
+import { LAYERMAP } from "@/classes/layerMap";
 import { Point2D } from "@/classes/point2D";
 import { ChangeEvent, useState } from "react";
 
 type ToolbarProps = {
     elementsManager: ElementsManager;
-    activeLayer: LayerType;
+    activeLayer: string;
 }
 
 export default function Toolbar({elementsManager, activeLayer}: ToolbarProps) {
     const [pathInput, setPathInput] = useState('');
-    const [helpText, setHelpText] = useState('Enter path as x1,y1;x2,y2;...');
 
     const handlePathInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
         setPathInput(e.target.value);
@@ -24,24 +24,18 @@ export default function Toolbar({elementsManager, activeLayer}: ToolbarProps) {
         return points;
     };
 
-    const handleAddFloor = () => {
-        const newPath = parsePathInput(pathInput);
-        elementsManager.addFloor(new Floor(newPath, 0));
-        elementsManager.draw(activeLayer);
-    };
-
     return (
         <div>
             <nav className="flex justify-left items-center p-2 bg-gray-200">
                 <button
                     className="px-4 py-2 text-lg cursor-pointer bg-blue-600 text-white border border-gray-300 rounded transition duration-300 hover:bg-blue-800"
-                    onClick={handleAddFloor}
+                    onClick={() => {}}
                 >
                     Add Floor
                 </button>
             </nav>
             <div className="mb-2 p-2 border border-gray-300 rounded w-full text-gray-400">
-                <p>{helpText}</p>
+                <p>{LAYERMAP[activeLayer].helperText}</p>
                 <textarea
                     className="no-border w-full focus:outline-none text-gray-500"
                     value={pathInput}
