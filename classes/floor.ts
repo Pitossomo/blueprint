e7b2d8cfa1f5c5b8e07e5ada447415f18c510b46
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-07 17:34:22 -0300

    feat(floor): Refactor constructor to use coordinates and dimensions instead of path

--- classes/floor.ts
+++ classes/floor.ts
@@ -1,32 +1,27 @@
 import { IElement } from "../app/interfaces/iElement"
-import { Point2D } from "./point2D";
 
 export class Floor implements IElement{
-    path: Point2D[];
+    x: number;
+    y: number;
+    dx: number;
+    dy: number;
     height: number;
     
-    constructor(path: Point2D[], height: number) {
-        // For testing purposes
-        /*
-        let startPoint = new Point2D(100 + height, 100 + height);
-        this.path = [
-            startPoint,
-            new Point2D(100 + height, 200 + height),
-            new Point2D(200 + height, 200 + height),
-            new Point2D(200 + height, 100 + height),
-            startPoint
-        ];
-        */
-        this.path = path;
+    constructor(x: number, y: number, dx: number, dy: number, height: number) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
         this.height = height;
     }
     
     draw(ctx: CanvasRenderingContext2D) {
-        if (this.path.length > 0) {
-            ctx.beginPath();
-            ctx.moveTo(this.path[0].x, this.path[0].y);
-            this.path.forEach((point) => { ctx.lineTo(point.x, point.y); }); 
-            ctx.stroke();
-        }
+        ctx.beginPath();
+        ctx.strokeRect(
+            this.x,
+            this.y,
+            this.dx,
+            this.dy
+        );
     }
 }
