0342a42f50360315b3f15c8b8e263a677feb163c
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-07 17:34:56 -0300

    feat(layerMap): Update layer descriptions to clarify input format for coordinates

--- classes/layerMap.ts
+++ classes/layerMap.ts
@@ -4,7 +4,7 @@ import { SlabList } from "./slabList";
 import { WallList } from "./wallList";
 
 export const LAYERMAP: Record<string,Layer> = {
-    FLOORS: new Layer("Pisos", "Informe os pontos como x1,y1 x2,y2 x3,y3 ...", new FloorList()),
-    SLABS: new Layer("Lajes", "Informe os pontos inicial e final como x1,y1 x2,y2", new SlabList()), 
-    WALL: new Layer("Paredes", "Informe os pontos inicial e final como x1,y1 x2,y2", new WallList()),
+    FLOORS: new Layer("Pisos", "Informe as extremidades do retângulo como x0,y0,comprimentoX,comprimentoY,altura", new FloorList()),
+    SLABS: new Layer("Lajes", "Informe as extremidades do retângulo como x0,y0,comprimentoX,comprimentoY,altura", new SlabList()), 
+    WALL: new Layer("Paredes", "Informe as extremidades do segmento reto como x1,y1 x2,y2", new WallList()),
 }
