411afa36ff6ef7211b1992e45a55e192114309fd
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-07 17:33:37 -0300

    feat(iElementList): Add parseInput method to interface

--- app/interfaces/iElementList.ts
+++ app/interfaces/iElementList.ts
@@ -4,4 +4,5 @@ export interface IElementList<T extends IElement> {
     elements: T[]
     draw(ctx: CanvasRenderingContext2D): void;
     add(element: T): void;
+    parseInput(input: string): void;
 }
