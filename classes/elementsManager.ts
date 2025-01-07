563f3484d0c90df55374627af9f512c76a453183
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-07 17:35:08 -0300

    feat(elementsManager): Remove unused parseInput method stub

--- classes/elementsManager.ts
+++ classes/elementsManager.ts
@@ -12,8 +12,4 @@ export class ElementsManager {
         const ctx = this.canvasRef.current?.getContext('2d');
         if (ctx) LAYERMAP[activeLayer].list.elements.forEach(el => {el.draw(ctx)});
     }
-
-    parseInput(input: string) {
-        //TODO
-    }
 }
