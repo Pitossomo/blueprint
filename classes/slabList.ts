f01d2749230321c02caa7e174e69bf1bf48c04cc
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-09 17:41:19 -0300

    feat(slabList): Enhance slab parsing and generation with direction handling

--- classes/slabList.ts
+++ classes/slabList.ts
@@ -1,9 +1,11 @@
+import { SlabDirection } from "@/app/enums/SlabDirection";
+import { FloorList } from "./floorList";
 import { Slab } from "./slab";
 
 export class SlabList {
     elements: Slab[] = [];
     
-    draw = (ctx: CanvasRenderingContext2D) => {
+    draw (ctx: CanvasRenderingContext2D) {
         this.elements.forEach(el => {el.draw(ctx)})
     }
 
@@ -11,13 +13,49 @@ export class SlabList {
         this.elements.push(newSlab);
         this.elements.sort((a, b) => a.x - b.x || a.y - a.y)
     }
+
+    private generateSlabDirection(dx: number, dy: number): SlabDirection  {
+        return dx < dy ? SlabDirection.X : SlabDirection.Y;
+    }
     
     parseInput(input: string): void {
-        const [x,y,dx,dy,height] = input.split(',').map(parseFloat);
+        const [x,y,dx,dy,height,direction] = input.split(',');
+        
+        let slabDirection: SlabDirection;
+        switch (direction.toUpperCase()) {
+            case "X":
+                slabDirection = SlabDirection.X;
+                break;
+            case "Y":
+                slabDirection = SlabDirection.Y;
+                break;
+            default:
+                slabDirection = this.generateSlabDirection(parseInt(dx), parseInt(dy));                
+        }
 
         try {
-          this.add(new Slab(x,y,dx,dy,height));
-        } catch (e) {
-        }
+            this.add(new Slab(
+                parseInt(x),
+                parseInt(y),
+                parseInt(dx),
+                parseInt(dy),
+                parseInt(height),
+                slabDirection
+            ));
+        } catch (e) {}
+    }
+
+    generateSlabs(floorList: FloorList): void {
+        this.elements = [];
+        floorList.elements.forEach(floor => {
+            this.add(new Slab(
+                floor.x,
+                floor.y,
+                floor.dx,
+                floor.dy,
+                floor.height,
+                this.generateSlabDirection(floor.dx, floor.dy)
+            ));
+        });
     }
 }
