22c24f552d97947e820c0bafc176fdefde76a843
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-10 21:14:37 -0300

    refactor(slabList): Improve input parsing for multiple lines and remove add method

--- classes/slabList.ts
+++ classes/slabList.ts
@@ -9,46 +9,49 @@ export class SlabList {
         this.elements.forEach(el => {el.draw(ctx)})
     }
 
-    add(newSlab: Slab) {
-        this.elements.push(newSlab);
-        this.elements.sort((a, b) => a.x - b.x || a.y - a.y)
-    }
-
     private generateSlabDirection(dx: number, dy: number): SlabDirection  {
         return dx < dy ? SlabDirection.X : SlabDirection.Y;
     }
     
     parseInput(input: string): void {
-        const [x,y,dx,dy,height,direction] = input.split(',');
-        
-        let slabDirection: SlabDirection;
-        switch (direction.toUpperCase()) {
-            case "X":
-                slabDirection = SlabDirection.X;
-                break;
-            case "Y":
-                slabDirection = SlabDirection.Y;
-                break;
-            default:
-                slabDirection = this.generateSlabDirection(parseInt(dx), parseInt(dy));                
-        }
+        const newSlabs: Slab[] = []; 
+        const lines = input.split('\n');
 
-        try {
-            this.add(new Slab(
-                parseInt(x),
-                parseInt(y),
-                parseInt(dx),
-                parseInt(dy),
-                parseInt(height),
-                slabDirection
-            ));
-        } catch (e) {}
+        lines.forEach(line => {
+            const [x,y,dx,dy,height,direction] = line.split(' ');
+        
+            let slabDirection: SlabDirection;
+            switch (direction.toUpperCase()) {
+                case "X":
+                    slabDirection = SlabDirection.X;
+                    break;
+                case "Y":
+                    slabDirection = SlabDirection.Y;
+                    break;
+                default:
+                    slabDirection = this.generateSlabDirection(parseFloat(dx), parseFloat(dy));                
+            }
+    
+            try {
+                newSlabs.push(new Slab(
+                    parseFloat(x),
+                    parseFloat(y),
+                    parseFloat(dx),
+                    parseFloat(dy),
+                    parseFloat(height),
+                    slabDirection
+                ));
+            } catch (e) {}
+        });
+
+        this.elements = newSlabs;
     }
 
     generateSlabs(floorList: FloorList): void {
+        const newSlabs: Slab[] = [];
         this.elements = [];
         floorList.elements.forEach(floor => {
-            this.add(new Slab(
+            newSlabs.push(new Slab(
                 floor.x,
                 floor.y,
                 floor.dx,
@@ -57,5 +60,6 @@ export class SlabList {
                 this.generateSlabDirection(floor.dx, floor.dy)
             ));
         });
+        this.elements = newSlabs;
     }
 }
