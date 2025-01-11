96695814007c5e9012616ec29a4729694315856a
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-10 21:13:21 -0300

    refactor(iElementList): Remove unused add method for cleaner interface

--- app/interfaces/iElementList.ts
+++ app/interfaces/iElementList.ts
@@ -3,6 +3,5 @@ import { IElement } from "./iElement";
 export interface IElementList<T extends IElement> {
     elements: T[]
     draw(ctx: CanvasRenderingContext2D): void;
-    add(element: T): void;
     parseInput(input: string): void;
 }
