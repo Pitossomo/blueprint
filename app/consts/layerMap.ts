9c02865e5139d6ade4442c5f96cb4932620238e7
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-10 21:13:47 -0300

    fix(layerMap): Update layer descriptions for clarity and consistency

--- app/consts/layerMap.ts
+++ app/consts/layerMap.ts
@@ -4,7 +4,7 @@ import { SlabList } from "../../classes/slabList";
 import { WallList } from "../../classes/wallList";
 
 export const LAYERMAP: Record<string,Layer> = {
-    FLOORS: new Layer("Pisos", "Informe as extremidades do retângulo como x0,y0,comprimentoX,comprimentoY,altura", new FloorList()),
-    SLABS: new Layer("Lajes", "Informe as extremidades do retângulo como x0,y0,comprimentoX,comprimentoY,altura", new SlabList()), 
-    WALL: new Layer("Paredes", "Informe as extremidades do segmento reto como x1,y1 x2,y2", new WallList()),
+    FLOORS: new Layer("Pisos", "Formato: x0 y0 dx dy h", new FloorList()),
+    SLABS: new Layer("Lajes", "Formato: x0 y0 dx dy h X/Y", new SlabList()), 
+    WALL: new Layer("Paredes", "Formato: x1 y1 x2 y2", new WallList()),
 }
