0b34f2debadf9716edee9c2e550bdda0d23292ba
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-11 16:10:04 -0300

    refactor(wallList): Improve input validation by removing try-catch and skipping invalid lines

--- classes/wallList.ts
+++ classes/wallList.ts
@@ -14,10 +14,8 @@ export class WallList {
         
         lines.forEach(line => {
             const [x1, y1, x2, y2] = line.split(' ').map(parseFloat);
-
-            try { 
-                newElements.push(new Wall(new Point2D(x1, y1), new Point2D(x2, y2)));
-            } catch (e) {}
+            if ([x1, y1, x2, y2].some(isNaN)) return;
+            newElements.push(new Wall(new Point2D(x1, y1), new Point2D(x2, y2)));
         });
 
         this.elements = newElements
