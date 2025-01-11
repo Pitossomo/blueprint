0856f884d8cc231dc0e7a6178434b01b5b71b9db
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-10 21:13:57 -0300

    refactor(floorList): Simplify input parsing and remove unused add method

--- classes/floorList.ts
+++ classes/floorList.ts
@@ -12,17 +12,18 @@ export class FloorList implements IElementList<Floor> {
         this.elements.forEach(el => {el.draw(ctx)})
     }
 
-    add(newFloor: Floor) {
-        this.elements.push(newFloor);
-        this.elements.sort((a,b) => a.height - b.height)
-    }
-
     parseInput(input: string): void {
-        const [x,y,dx,dy,height] = input.split(',').map(parseFloat);
+        const newElements: Floor[] = []; 
+        const lines = input.split('\n');
+        
+        lines.forEach(line => {
+            const [x,y,dx,dy,height] = line.split(' ').map(parseFloat);
 
-        try {
-          this.add(new Floor(x,y,dx,dy,height));
-        } catch (e) {
-        }
+            try {
+                newElements.push(new Floor(x,y,dx,dy,height));
+            } catch (e) {}
+        });
+
+        this.elements = newElements
     }
 }
