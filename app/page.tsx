d2e986d964a8a59ff88f0a03cbfad59d1b1d1e2f
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-13 17:50:37 -0300

    feat(page): Integrate SelectInput for layer and floor level selection

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,23 @@ import { ElementsManager } from "@/classes/elementsManager";
 import { useState, useRef, ChangeEvent, useEffect } from "react";
 import Toolbar from "./components/toolbar";
 import { LAYERMAP } from "@/app/consts/layerMap";
+import SelectInput from "./components/selectInput";
 
 export default function Home() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const elementsManager = new ElementsManager(canvasRef);
   const [activeLayer, setActiveLayer] = useState<string>("FLOORS");
+  const [floorLevel, setFloorLevel] = useState<number>(0);
 
   const handleLayerChange = (e: ChangeEvent<HTMLSelectElement>): void => {
     const selectedLayer = e.target.value;
     setActiveLayer(selectedLayer)
   };
+
+  const handleFloorLevelChange = (e: ChangeEvent<HTMLSelectElement>): void => {
+    const selectedFloorLevel = parseInt(e.target.value);
+    setFloorLevel(selectedFloorLevel);
+  }
 
   useEffect(() => {
     elementsManager.draw(activeLayer); // Redraw the canvas
@@ -25,17 +32,24 @@ export default function Home() {
 
       <div className="relative border border-gray-300 rounded w-full overflow-hidden">
         <canvas
-          className="w-full h-full"
           width="1000" height="600"
           ref={canvasRef}
         />
       </div>
 
-      <select className="p-4 border border-gray my-2" onChange={handleLayerChange}>
-        { Object.entries(LAYERMAP).map(([layerKey, layerObj]) => (
-          <option key={layerKey} value={layerKey}>{layerObj.label}</option>
-        ))}
-      </select>
+      <div className="flex">
+        <SelectInput label="Selecione a camada:" onChange={handleLayerChange}>
+          { Object.entries(LAYERMAP).map(([layerKey, layerObj]) => (
+            <option key={layerKey} value={layerKey}>{layerObj.label}</option>
+          ))}
+        </SelectInput>
+
+        <SelectInput label="Selecione o pavimento" onChange={handleFloorLevelChange}>
+          <option value="0">Térreo</option>
+          <option value="300">1º Andar</option>
+          <option value="600">2º Andar</option>
+        </SelectInput>
+      </div>
 
       <Toolbar elementsManager={elementsManager} activeLayer={activeLayer} />
     </div>
