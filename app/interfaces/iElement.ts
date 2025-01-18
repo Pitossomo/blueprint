e370a2a7d13728287b0d00d2b48a4e298220c0ef
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-18 07:07:22 -0300

    feat(element): Update IElement interface to include activeLevel parameter for draw method

--- app/interfaces/iElement.ts
+++ app/interfaces/iElement.ts
@@ -1,3 +1,5 @@
+import { Level } from "@/classes/level";
+
 export interface IElement {
-    draw(ctx: CanvasRenderingContext2D): void;
+    draw(ctx: CanvasRenderingContext2D, activeLevel: Level): void;
 }
