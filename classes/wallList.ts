70b81c4a7cdaa483ce19ef2e926be8238f6af5b5
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-18 07:12:01 -0300

    feat(wallList): Implement IElementList interface

--- classes/wallList.ts
+++ classes/wallList.ts
@@ -1,23 +1,32 @@
-import { Point2D } from "./point2D";
+import { IElementList } from "@/app/interfaces/iElementList";
+import { Level } from "./level";
 import { Wall } from "./wall";
 
-export class WallList {
-    elements: Wall[] = [];
+export class WallList implements IElementList<Wall> {
+    private elements: Wall[] = [];
     
     draw = (ctx: CanvasRenderingContext2D) => {
         this.elements.forEach(el => {el.draw(ctx)})
     }
 
-    parseInput(input: string): void {
+    parseInput(input: string, activeLevel: Level): void {
         const newElements: Wall[] = []; 
         const lines = input.split('\n');
         
         lines.forEach(line => {
             const [x1, y1, x2, y2] = line.split(' ').map(parseFloat);
             if ([x1, y1, x2, y2].some(isNaN)) return;
-            newElements.push(new Wall(new Point2D(x1, y1), new Point2D(x2, y2)));
+            newElements.push(new Wall(x1, y1, x2, y2, activeLevel));
         });
-
         this.elements = newElements
     }
+
+    getInput(activeLevel: Level): string {
+        return this.elements.map(el => (el.getLevel() === activeLevel)
+            ? `${el.getX1()} ${el.getY1()} ${el.getX2()} ${el.getY2()}`
+            : ''
+        ).join('\n');
+    }
+
+    getElements(): Wall[] { return this.elements; }
 }
