2e830393863405383cfd69825815d2a4e72b4133
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-18 07:13:13 -0300

    feat(layerMap): Rename attributes for consistency

--- app/consts/layerMap.ts
+++ app/consts/layerMap.ts
@@ -3,8 +3,8 @@ import { Layer } from "../../classes/layer";
 import { SlabList } from "../../classes/slabList";
 import { WallList } from "../../classes/wallList";
 
-export const LAYERMAP: Record<string,Layer> = {
+export const LAYER_MAP: Record<string,Layer> = {
     FLOORS: new Layer("Pisos", "Formato: x0 y0 dx dy h", new FloorList()),
     SLABS: new Layer("Lajes", "Formato: x0 y0 dx dy h X/Y", new SlabList()), 
-    WALL: new Layer("Paredes", "Formato: x1 y1 x2 y2", new WallList()),
+    WALLS: new Layer("Paredes", "Formato: x1 y1 x2 y2", new WallList()),
 }
