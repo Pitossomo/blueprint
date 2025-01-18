148eb888e5cb56e422252ee74bdae6deb10d0ad7
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-18 06:58:40 -0300

    feat(wall): Refactor Wall class encapsulation via access modificators

--- classes/wall.ts
+++ classes/wall.ts
@@ -1,16 +1,29 @@
-import { Point2D } from "./point2D";
+import { Level } from "./level";
 
 export class Wall {
-    start: Point2D;
-    end: Point2D;
+    private x1: number;
+    private x2: number;
+    private y1: number;
+    private y2: number;
+    level: Level;
 
-    constructor(start: Point2D, end: Point2D) {
-        [this.start, this.end] = start.x < end.x ? [start, end] : [end, start];
+    constructor(x1: number, y1: number, x2: number, y2: number, level: Level) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.level = level;
     }
 
     draw(ctx: CanvasRenderingContext2D) {
         ctx.beginPath();
-        ctx.moveTo(this.start.x, this.start.y);
-        ctx.lineTo(this.end.x, this.end.y);
+        ctx.moveTo(this.x1, this.y1);
+        ctx.lineTo(this.x2, this.y2);
     }
+
+    getX1(): number { return this.x1; }
+    getX2(): number { return this.x2; }
+    getY1(): number { return this.y1; }
+    getY2(): number { return this.y2; }
+    getLevel(): Level { return this.level; }
 }
