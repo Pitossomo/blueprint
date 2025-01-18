c1fd05cf0982bb50c48ce65ac4d23bf7c6e21881
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-18 06:58:59 -0300

    feat(slab): Enhance Slab class encapsulation

--- classes/slab.ts
+++ classes/slab.ts
@@ -1,24 +1,29 @@
 import { SlabDirection } from "@/app/enums/SlabDirection";
 import { IElement } from "../app/interfaces/iElement";
+import { Level } from "./level";
 
 export class Slab implements IElement {
-    x: number;
-    y: number;
-    dx: number;
-    dy: number;
-    height: number;
-    direction: SlabDirection;
+    private x: number;
+    private y: number;
+    private dx: number;
+    private dy: number;
+    private height: number;
+    private direction: SlabDirection;
+    private level: Level;
     
-    constructor(x: number, y: number, dx: number, dy: number, height: number, slabDirection: SlabDirection) {
+    constructor(x: number, y: number, dx: number, dy: number, level: Level, height: number, slabDirection: SlabDirection) {
         this.x = x;
         this.y = y;
         this.dx = dx;
         this.dy = dy;
         this.height = height;
         this.direction = slabDirection;
+        this.level = level;
     }
     
-    draw(ctx: CanvasRenderingContext2D) {
+    draw(ctx: CanvasRenderingContext2D, activeLevel: Level): void {
+        if (activeLevel !== this.level) return;
+
         ctx.strokeRect(this.x, this.y, this.dx, this.dy);
         const [xOffset, yOffset] = this.direction === SlabDirection.X 
             ? [0, this.dx / 4]
@@ -29,4 +34,12 @@ export class Slab implements IElement {
         ctx.lineTo(endX, endY);
         ctx.stroke();
     }
+
+    getX(): number { return this.x; }
+    getY(): number { return this.y; }
+    getDX(): number { return this.dx; }
+    getDY(): number { return this.dy; }
+    getHeight(): number { return this.height; }
+    getLevel(): Level { return this.level; }
+    getDirection(): SlabDirection { return this.direction; }
 }
