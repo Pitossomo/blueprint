c7e73fdb9f44089d8398e804a10c21962eb97e68
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-22 06:19:13 -0300

    feat(beam.ts): Add Beam class to represent structural beams with drawing and equality methods

---
+++ classes/beam.ts
@@ -0,0 +1,46 @@
+import { IElement } from "@/app/interfaces/iElement";
+import { Level } from "./level";
+
+export class Beam implements IElement {
+    private x1: number;
+    private y1: number;
+    private x2: number;
+    private y2: number;
+    private level: Level;
+    heightFromLevel: any;
+    
+    constructor(x1: number, y1: number, x2: number, y2: number, heightFromLevel: number, level: Level) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.level = level;
+        this.heightFromLevel = heightFromLevel;
+    }
+    draw(ctx: CanvasRenderingContext2D, activeLevel: Level): void {
+        if (activeLevel !== this.level) return;
+
+        ctx.beginPath();
+        ctx.moveTo(this.x1, this.y1);
+        ctx.lineTo(this.x2, this.y2);
+        ctx.stroke();
+    }
+
+    equals(other: Beam): boolean {
+        return (
+            this.x1 === other.x1 &&
+            this.y1 === other.y1 &&
+            this.x2 === other.x2 &&
+            this.y2 === other.y2 &&
+            this.level === other.level &&
+            this.heightFromLevel === other.heightFromLevel
+        );
+    }
+
+    getX1(): number { return this.x1; }
+    getX2(): number { return this.x2; }
+    getY1(): number { return this.y1; }
+    getY2(): number { return this.y2; }
+    getLevel(): Level { return this.level; }
+    getHeightFromLevel(): number { return this.heightFromLevel; }
+}
