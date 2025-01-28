fa4aa5c4957b1e2e551751d58f32ff4a70cc86dd
Pitossomo <pedro.h.teles@hotmail.com>
2025-01-28 17:25:47 -0300

    feat(beam.ts): Enhance Beam class to manage nodes and improve constructor logic

--- classes/beam.ts
+++ classes/beam.ts
@@ -1,5 +1,6 @@
 import { IElement } from "@/app/interfaces/iElement";
 import { Level } from "./level";
+import { Node } from './node'
 
 export class Beam implements IElement {
     private x1: number;
@@ -7,14 +8,25 @@ export class Beam implements IElement {
     private x2: number;
     private y2: number;
     private level: Level;
-    heightFromLevel: any;
+    private heightFromLevel: any;
+    private nodes: Node[];
     
     constructor(x1: number, y1: number, x2: number, y2: number, heightFromLevel: number, level: Level) {
-        this.x1 = Math.min(x1,x2);
-        [this.y1,this.x2,this.y2] = (x1 === this.x1) ? [y1,x2,y2] : [y2,x1,y1];
-        this.level = level;
-        this.heightFromLevel = heightFromLevel;
+        if (x1 === x2) {
+            if (y1 === y2) throw Error;
+            else [this.x1,this.y1,this.x2,this.y2] = [x1, Math.min(y1,y2), x2, Math.max(y1,y2)]
+        } else {
+            this.x1 = Math.min(x1,x2);
+            [this.y1,this.x2,this.y2] = (x1 === this.x1) ? [y1,x2,y2] : [y2,x1,y1];
+        }
+            this.level = level;
+            this.heightFromLevel = heightFromLevel;
+            this.nodes = [
+                new Node(this.x1, this.y1, this.heightFromLevel, this.level),
+                 new Node(this.x2, this.y2, this.heightFromLevel, this.level)
+            ]
     }
+
     draw(ctx: CanvasRenderingContext2D, activeLevel: Level): void {
         if (activeLevel !== this.level) return;
 
@@ -35,6 +47,16 @@ export class Beam implements IElement {
         );
     }
 
+    addNode(newNode: Node) {
+        this.nodes.push(newNode)
+        this.nodes.sort((a,b) => {
+            return (
+                a.getX() - b.getX()
+                || a.getY() - b.getY()
+            )
+        })
+    }
+
     getX1(): number { return this.x1; }
     getX2(): number { return this.x2; }
     getY1(): number { return this.y1; }
@@ -44,4 +66,5 @@ export class Beam implements IElement {
     getY0(): number { return this.y1 - this.getLinearCoefficient()*this.x1}
     getLevel(): Level { return this.level; }
     getHeightFromLevel(): number { return this.heightFromLevel; }
+    getNodes(): Node[] { return this.nodes }
 }
