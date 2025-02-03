c3a80a1e42e7cc8b4c5bd7b864511461a428ee28
Pitossomo <pedro.h.teles@hotmail.com>
2025-02-03 17:22:16 -0300

    feat(superficialLoad.ts): Add SuperficialLoad class to manage load parameters

---
+++ classes/superficialLoad.ts
@@ -0,0 +1,11 @@
+export class SuperficialLoad {
+    permanent: number;
+    accidental: number;
+
+    constructor(permanent: number, accidental: number) {
+        this.permanent = permanent;
+        this.accidental = accidental;
+    }
+}
+
+export default SuperficialLoad;
