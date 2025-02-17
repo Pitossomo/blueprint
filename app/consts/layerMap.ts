6298c71eab90d905732b8f643b1f0ced4594b3b8
Pitossomo <pedro.h.teles@hotmail.com>
2025-02-17 14:01:40 -0300

    feat: Add columns layer to LayerMap for managing Column elements

--- app/consts/layerMap.ts
+++ app/consts/layerMap.ts
@@ -1,16 +1,18 @@
-import BeamList from "@/classes/beamList";
-import FloorList from "../../classes/floorList";
-import Layer from "../../classes/layer";
-import SlabList from "../../classes/slabList";
-import WallList from "../../classes/wallList";
+import BeamList from "@/classes/structures/beamList";
+import FloorList from "@/classes/floorList";
+import Layer from "@/classes/layer";
+import SlabList from "@/classes/structures/slabList";
+import WallList from "@/classes/wallList";
 import IElementList from "../interfaces/iElementList";
 import IElement from "../interfaces/iElement";
+import ColumnList from "@/classes/structures/columnsList";
 
 class LayerMap {
     readonly floors: Layer<FloorList>;
     readonly slabs: Layer<SlabList>;
     readonly walls: Layer<WallList>;
     readonly beams: Layer<BeamList>;
+    readonly columns: Layer<ColumnList>
     private layers: Record<string, Layer<IElementList<IElement>>>
 
     constructor () {
@@ -18,11 +20,13 @@ class LayerMap {
         this.slabs = new Layer("Lajes", "Formato: x0 y0 dx dy h X/Y", new SlabList());
         this.walls = new Layer("Paredes", "Formato: x1 y1 x2 y2", new WallList());
         this.beams = new Layer("Vigas", "Formato: x1 y1 x2 y2 h", new BeamList());
+        this.columns = new Layer("Pilares", "Formato: x y n°NívelInicial n°nívelFinal", new ColumnList());
         this.layers = {
             floors: this.floors,
             slabs: this.slabs,
             walls: this.walls,
-            beams: this.beams
+            beams: this.beams,
+            columns: this.columns,
         }  
     }
 
