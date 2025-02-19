c8d557da55dc5968bb721d8f3d9ca13da5822ead
Pitossomo <pedro.h.teles@hotmail.com>
2025-02-18 22:12:59 -0300

    feat: Update Level constructor to include additional parameter for level index

--- app/consts/levelMap.ts
+++ app/consts/levelMap.ts
@@ -1,8 +1,8 @@
 import Level from "@/classes/level"
 
 export const LEVEL_LIST: Level[] = [
-    new Level("Térreo", 0),
-    new Level("Superior", 306),
-    new Level("Terraço", 612),
-    new Level("Cobertura", 918),
+    new Level("Térreo", 0, 0),
+    new Level("Superior", 306, 1),
+    new Level("Terraço", 612, 2),
+    new Level("Cobertura", 918, 3),
 ]
