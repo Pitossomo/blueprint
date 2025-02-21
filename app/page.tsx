0a0cd4dda13938e208d2d1d97336ba2841e51735
Pitossomo <pedro.h.teles@hotmail.com>
2025-02-21 06:02:17 -0300

    feat: Replace SelectInput components with ActiveOptions for layer and level selection

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import ElementsManager from "@/classes/elementsManager";
-import { useState, useRef, ChangeEvent, useEffect } from "react";
-import SelectInput from "./components/selectInput";
+import { useState, useRef, useEffect } from "react";
 import Level from "@/classes/level";
 import { LAYER_MAP } from "@/app/consts/layerMap";
 import { LEVEL_LIST } from "@/app/consts/levelMap";
@@ -11,6 +10,7 @@ import IElementList from "./interfaces/iElementList";
 import IElement from "./interfaces/iElement";
 import dynamic from "next/dynamic";
 import Toolbar from "./components/toolbar";
+import ActiveOptions from "./components/activeOptions";
 
 const Canvas = dynamic(() => import("./components/canvas"), {ssr: false});
 
@@ -27,16 +27,6 @@ export default function Home() {
 		setCommandLineInput(getInput(activeLayer, activeLevel));
 	}, [activeLayer, activeLevel]);
 
-	function handleLayerChange (e: ChangeEvent<HTMLSelectElement>): void {
-		const selectedLayer = LAYER_MAP.getLayer(e.target.value);
-    	setActiveLayer(selectedLayer);
-	};
-
-	function handleFloorLevelChange (e: ChangeEvent<HTMLSelectElement>): void {
-    	const selectedLevel = LEVEL_LIST[parseInt(e.target.value)];
-		setActiveLevel(selectedLevel);
-	}
-
 	const getInput = (layer: Layer<IElementList<IElement>>, level: Level) => { return elementsManager.getInput(layer, level); }
 
 	const redraw = () => { 
@@ -52,19 +42,7 @@ export default function Home() {
       		<h1 className="text-2xl font-bold mb-4">Desenhe Formas Geométricas</h1>
     		<Canvas canvasRef={canvasRef} />
       		
-			<div className="flex">
-				<SelectInput label="Selecione a camada:" onChange={handleLayerChange}>
-					{ LAYER_MAP.getEntries().map(([layerKey, layerObj]) => (
-						<option key={layerKey} value={layerKey}>{layerObj.getLabel()}</option>
-          			))}
-        		</SelectInput>
-
-        		<SelectInput label="Selecione o pavimento" onChange={handleFloorLevelChange}>
-					{ LEVEL_LIST.map((level, index) => (
-						<option key={level.getName()} value={index}>{level.getName()}</option>
-		  			))}          			
-        		</SelectInput>
-      		</div>
+			<ActiveOptions setActiveLayer={setActiveLayer} setActiveLevel={setActiveLevel} />
 
 			<div>
 				<Toolbar
