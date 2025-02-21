b038a7d4e9974e9c7659adda7afa40e54460ac67
Pitossomo <pedro.h.teles@hotmail.com>
2025-02-21 06:52:03 -0300

    feat: Refactor Layer class to improve visibility handling and add draw method for layer rendering

--- classes/layer.ts
+++ classes/layer.ts
@@ -1,17 +1,18 @@
 import IElement from "@/app/interfaces/iElement";
 import IElementList from "@/app/interfaces/iElementList";
+import Level from "./level";
 
 export default class Layer<T extends IElementList<IElement>> {
     private label: string;
     private helperText: string;
     private list: T;
-    private isVisible: boolean;
+    private visibility: boolean;
 
     constructor(label: string, helperText: string, list: T) {
         this.label = label;
         this.helperText = helperText;
         this.list = list;
-        this.isVisible = false;
+        this.visibility = false;
     }
 
     setLabel(label: string) { this.label = label; }
@@ -19,6 +20,15 @@ export default class Layer<T extends IElementList<IElement>> {
     setHelperText(helperText: string) { this.helperText = helperText; }
     getHelperText() { return this.helperText; }
     getList() { return this.list; }
-    setVisibility(newVisibility: boolean) { this.isVisible = newVisibility }
-    getVisibility() { return this.isVisible }
+    setVisibility(newVisibility: boolean) { this.visibility = newVisibility }
+    isVisible() { return this.visibility }
+    draw(ctx: CanvasRenderingContext2D, activeLevel: Level, activeLayer: Layer<IElementList<IElement>>) {
+        const isLayerActive = (this === activeLayer) 
+        this.list.draw(ctx, activeLevel, isLayerActive)
+    }
+    parseInput(input: string, activeLevel: Level) { this.list.parseInput(input, activeLevel) };
+    getInput(activeLevel: Level) { return this.list.getInput(activeLevel) };
+    getElements() { return this.list.getElements() };
+    getBoundingBox() { return this.list.getBoundingBox() };
+    copyToOtherLevels(activeLevel: Level) {this.list.copyToOtherLevels(activeLevel) }    
 }
