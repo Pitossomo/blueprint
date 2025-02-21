b9568ede254da0bfa08d27d846bdfb36049d710d
Pitossomo <pedro.h.teles@hotmail.com>
2025-02-21 06:26:20 -0300

    feat: Add visibility management to Layer class with setVisibility and getVisibility methods

--- classes/layer.ts
+++ classes/layer.ts
@@ -5,11 +5,13 @@ export default class Layer<T extends IElementList<IElement>> {
     private label: string;
     private helperText: string;
     private list: T;
+    private isVisible: boolean;
 
     constructor(label: string, helperText: string, list: T) {
         this.label = label;
         this.helperText = helperText;
         this.list = list;
+        this.isVisible = false;
     }
 
     setLabel(label: string) { this.label = label; }
@@ -17,4 +19,6 @@ export default class Layer<T extends IElementList<IElement>> {
     setHelperText(helperText: string) { this.helperText = helperText; }
     getHelperText() { return this.helperText; }
     getList() { return this.list; }
+    setVisibility(newVisibility: boolean) { this.isVisible = newVisibility }
+    getVisibility() { return this.isVisible }
 }
